7bd71d12080357142b7d0738bfc488a14676e2d1
DanielTakeshi <takeshidanny@gmail.com>
2015-04-21 12:52:36 -0700

    Got t=2 working. Now have to check the points

--- frontier.py
+++ frontier.py
@@ -25,14 +25,14 @@ import sys
 # MAIN #
 ########
 
-gran = 60
-if len(sys.argv) == 3:
-    gran = int(sys.argv[2])
+gran = 24
+if len(sys.argv) == 2:
+    gran = int(sys.argv[1])
 if gran <= 0 or gran >= 100:
     print "Granularity of " + str(gran) + " would cause problems."
     sys.exit()
 
-T = 1
+T = 2
 # H[t][r0,r1] gives the minimum r2 (or NaN) for making <r0,r1,r2> optimal in t-round game.
 H = [np.nan] # Putting NaN here as dummy first matrix, because we only want t=1,2,...
 lossPatterns = np.array([[0,0,0],
@@ -70,33 +70,37 @@ for t in range(1,T+1):
                 realizable = False
                 for p0 in range(0, gran+1):
                     for p1 in range(0, gran-p0+1):
-                        p2 = gran-p0-p1
 
+                        p2 = gran-p0-p1
                         # First do an easy check if adversary can send any component negative
                         if (r0 < p1+p2) or (r1 < p0+p2) or (r2 < p0+p1):
                             continue
-
                         # If t=1, then we already know we are safe
                         if t == 1:
                             realizable = True
                             break
-
-                        # Efficiently compute 7 ways where adversary could "send" <r0,r1,r2>.
+                        # Else, for t>1, efficiently compute 7 ways where adversary can "send" <r0,r1,r2>.
                         ix1 = np.tile(np.array([r0,r1,r2]), (7,1))
-                        ix2 = gran*lossPatterns - np.tile(np.array(lossPatterns * [[p0],[p1],[p2]]), (1,3))
+                        ix2 = gran*lossPatterns - np.tile(np.dot(lossPatterns, np.array([[p0],[p1],[p2]])), (1,3))
                         ix = ix1 - ix2
-                        # TODO Not finished with this but I guess since this is only for T > 1...
-                        # if (...) realizable=True, break
+                        # Clamp so that we can call same indices using H[t-1] for the testing
+                        ix = np.clip(ix, a_min=None, a_max=(gran*(t-1)))
+                        r0_coords = ix[:,0].ravel().astype(int)
+                        r1_coords = ix[:,1].ravel().astype(int)
+                        # If there are NaNs in the previous matrix, we know <r0,r1,r2>,<p0,p1,p2> can't happen.
+                        nan_indices = np.where( np.isnan(H[t-1][r0_coords,r1_coords]) )[0]
+                        if nan_indices.size == 0:
+                            if np.all( H[t-1][r0_coords,r1_coords] <= ix[:,2] ):
+                                realizable = True
+                                break
 
                     if realizable:
                         break
-
                 # Still in while loop, but finished with looping through <p0,p1,p2>
                 if not realizable: # This means NO <p0,p1,p2> works for this <r0,r1,r2>. Also if r2 = -1.
                     doneWithDecrements = True
                 else:
                     r2 = r2 - 1
-
             # Finished with while loop, have largest r2 that makes <r0,r1,r2> unrealizable
             if r2 == gran*t:
                 H[t][r0,r1] = np.nan
@@ -107,8 +111,13 @@ for t in range(1,T+1):
 # Set up some plots, being sure to divide by 'gran' at the end!
 print "\nAll done with generating points. Now time to plot."
 scatter = True
+surface = False
+save_to_files = True
 for t in range(1,T+1):
+
     data = H[t]
+    if save_to_files:
+        np.savetxt("points_" + str(t) + "_" + str(gran), data, fmt='%2.f')
     x = np.array([])
     y = np.array([])
     z = np.array([])
@@ -126,11 +135,11 @@ for t in range(1,T+1):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         ax.scatter(x,y,z)
-        ax.set_xlim([0,1])
-        ax.set_ylim([0,1])
-        ax.set_zlim([0,1])
+        ax.set_xlim([0,t])
+        ax.set_ylim([0,t])
+        ax.set_zlim([0,t])
         plt.show()
 
-
-
+    if surface:
+        print "Not implemented yet"
 
